6a015a34c8a0ed1d2d12031a3edcf1f86ea59184
Younes SERRA <yserra@corp.free.fr>
2022-08-22 00:04:29 +0200

    added lottie + animation login/registration pages

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -1,18 +1,19 @@
 <script setup>
-import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/inertia-vue3';
+import {Link} from '@inertiajs/inertia-vue3';
+import {Vue3Lottie} from "vue3-lottie";
+import Panda from "../Components/Lottie/panda.json";
 </script>
 
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <Link href="/">
-                <BreezeApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <Vue3Lottie :animationData="Panda" :loop="false" class="w-1/2 mx-auto"/>
             </Link>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <slot />
+        <div class="w-full sm:max-w-md  px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <slot/>
         </div>
     </div>
 </template>
